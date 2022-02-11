e737fc57142e77d67bc870902d22c67e91022e53
DrKoop <95058605+DrKoop@users.noreply.github.com>
2022-02-10 19:20:38 -0600

    Delete 03-app.js

--- js/03-app.js
+++
@@ -1,9 +0,0 @@
-const producto = 'monitor 20 pulgadas' ;
-const precio = " 30 usd ";
-
-/* console.log(producto.concat(precio)) */
-console.log(producto.concat('  chingue su madre el negro'))
-console.log(producto + precio + 'huevos')
-//template strings, una forma moderna, o "mejor" de concatenar strings, solo es util con la comila backtick  ``
-console.log(`el producto ${producto} tiene un precio de ${precio} mi perroooooooo `);
-console.log(producto , precio , 'puto el que lo leea')
